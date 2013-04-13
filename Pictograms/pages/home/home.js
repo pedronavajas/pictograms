e8315d5db991a7d587060cb814a8d86567eec6a9
drinor <rjurado@noslosoftware.biz>
2013-04-13 16:52:46 +0200

    Cambios de estructura.

--- Pictograms/pages/home/home.js
+++ Pictograms/pages/home/home.js
@@ -6,6 +6,29 @@
         // rellena los elementos de la página con los datos de la aplicación.
         ready: function (element, options) {
             // TODO: Inicializar la página aquí.
+
+            var buttom = document.getElementById("exit");
+            buttom.addEventListener("click", this.changeItem, false);
+
+            var buttom2 = document.getElementById("page2");
+            buttom2.addEventListener("click", this.goToPage2, false);
+
+            WinJS.Application.sessionState.selectedArray = WinJS.Binding.as({
+                item1: "/images/pictograms/vacio.png",
+                item2: null,
+                item3: null,
+                item4: null
+            });
+
+            WinJS.Binding.processAll(document.body, WinJS.Application.sessionState.selectedArray);
+        },
+
+        changeItem: function (eventInfo) {
+            WinJS.Application.sessionState.selectedArray.item1 = "/images/pictograms/ok.png";
+        },
+
+        goToPage2: function (eventInfo) {
+            WinJS.Navigation.navigate("/pages/page2/pagecontrol.html");
         }
     });
 })();
